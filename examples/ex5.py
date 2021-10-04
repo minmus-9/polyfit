645650e492aa6163509bb5a4cedce858c80df02d
cave canem <cave-canem@waggly.dog>
2021-10-04 16:56:03 -0700

    * pull ex5  mods

--- examples/ex5.py
+++ examples/ex5.py
@@ -14,7 +14,7 @@ import sys
 sys.path.insert(0, "..")
 
 from polyfit import (   ## pylint: disable=wrong-import-position
-    PolyfitPlan, to_float
+    PolyfitPlan, to_float, to_quad, zero, one, add, mul
 )
 
 from cholesky import chofit     ## pylint: disable=wrong-import-position
@@ -22,7 +22,7 @@ from cholesky import chofit     ## pylint: disable=wrong-import-position
 
 def flist(l):
     "format a list to 15 decimal places"
-    if not isinstance(l, list):
+    if not isinstance(l, (list, tuple)):
         l = [l]
     return " ".join("%.15e" % x for x in l)
 
@@ -31,29 +31,27 @@ def demo():
     ## pylint: disable=unnecessary-comprehension,too-many-locals
 
     ## poly coefficients to fit, highest degree first
-    cv = [math.sqrt(2), math.exp(1), math.pi, 1]
+    cv   = [2, 1, -1, math.pi]
+    cvee = [to_quad(z) for z in cv]
+    #cv   = [to_quad(z) for z in [math.sqrt(2), math.exp(1), math.pi, 1]]
 
     ## evaluate the polynomial above using horner's method
     def pv(x):
         "evaluate using cv"
-        r = 0.
-        for c in cv:
-            r *= x
-            r += c
+        x = to_quad(x)
+        r = zero()
+        for c in cvee:
+            r = add(mul(r, x), c)
         return r
 
     ## define the x and y values for the fit
     N  = 10000
-    xv = [x for x in range(N)]
+    xv = [to_quad(x) for x in range(N)]
     yv = [pv(x) for x in xv]
 
     ## weights:
     ##     uniform to minimize the max residual
-    wv = [1. for _ in xv]
-
-    ##     relative to minimize the relative residual
-    ##     note that y is nonzero for this example
-    #wv = [y ** -2. for y in yv]
+    wv = [one() for _ in xv]
 
     ## perform the fit
     D    = len(cv) - 1
@@ -61,24 +59,33 @@ def demo():
     fit  = plan.fit(yv)
     ev   = fit.evaluator()
 
-    ## print the fit stats
-    deg = plan.maxdeg()
-    print("maxdeg", deg)
-    print("points", plan.npoints())
-
     ## print rel errs in coefs
-    obs = ev.coefs(xv[0], -1)
-    rel = [abs(o/e - 1.) for o, e in zip(obs, cv)]
+    obs = [to_float(c) for c in ev.coefs(xv[0], -1)]
+    rel = [abs(o/e - 1.) if e else o for o, e in zip(obs, cv)]
     print("polyfit:")
     print("  coefs ", flist(obs))
     print("  relerr", flist(rel))
+    print("    P(0)", flist(ev(zero())))
+    print(" P(N>>1)", flist(ev(to_quad(N >> 1))))
+    print("     exp", flist(pv(N >> 1)))
 
     ## numpy
-    obs = [to_float(c) for c in chofit(xv, yv, wv, D)]
-    rel = [abs(o/e - 1.) for o, e in zip(obs, cv)]
+    cof = chofit(xv, yv, wv, D)
+    def nv(x):
+        x = to_quad(x)
+        r = zero()
+        for c in cof:
+            r = add(mul(r, x), c)
+        return r
+
+    obs = [to_float(c) for c in cof]
+    rel = [abs(o/e - 1.) if e else o for o, e in zip(obs, cv)]
     print("numpy:")
     print("  coefs ", flist(obs))
     print("  relerr", flist(rel))
+    print("    P(0)", flist(nv(0)))
+    print(" P(N>>1)", flist(nv(N >> 1)))
+    print("     exp", flist(pv(N >> 1)))
 
 if __name__ == "__main__":
     demo()
